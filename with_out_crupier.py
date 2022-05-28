86a0c151fb7c96041496403342acd2a3f1fa9425
dimmy1 <dmytrobuzan@gmail.com>
2022-05-28 15:23:24 +0100

    first commit

---
+++ with_out_crupier.py
@@ -0,0 +1,31 @@
+import random
+from Deck import deck
+
+
+random.shuffle(deck)
+
+
+print("Let's play?")
+count = 0
+
+
+While True:
+    choice = input('Will you take a card? Yes/No\n')
+    if choise == "Yes":
+        current = deck.pop()
+        print("You got a card of value: %d" %current)
+        count += current
+        if count > 21:
+            print('Sorry, but you lose!')
+            break
+        elif count == 21:
+            print('Congratulations, you scored 21!')
+            break
+        else:
+            print('You score %d points.' %count)
+    elif choise == "No":
+        print('You score %d points and you finish the game!' %count)
+        break
+
+
+print("See you soon!")
